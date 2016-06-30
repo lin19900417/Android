b90484f2ed6ba53c5c01b22caa84eb0b60b30047
lin19900417 <827870138@qq.com>
2016-06-30 15:58:54 +0800

    添加一个按钮

---
+++ app/src/main/java/com/google/administrator/android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.google.administrator.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
